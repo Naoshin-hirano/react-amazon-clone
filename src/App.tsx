1387bea7a7314b70b8d8fc5fb163905c2b761852
Hirano <hnaoya1120@gmail.com>
2022-09-19 15:12:10 +0900

    Atomic Designでの雛形

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Outlet, Router } from "@tanstack/react-location";
+import { Provider } from "react-redux";
+import { store } from "./core/store";
+import { location, routes } from "./route";
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Provider store={store}>
+            <Router routes={routes} location={location}>
+                <Outlet />
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
